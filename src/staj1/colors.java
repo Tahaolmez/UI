90f54895b8ee40ea9c769c56e72f849590ff8072
Tahaolmez <111271041+Tahaolmez@users.noreply.github.com>
2022-08-15 10:51:09 +0300

    Add files via upload
    main

---
+++ src/staj1/colors.java
@@ -0,0 +1,11 @@
+package staj1;
+import java.awt.Color;
+public class colors {
+	
+	Color darkpanel = new Color(170,170,170);
+	Color darksubpanel = new Color(140,140,140);
+	Color darkcomponents = new Color(130,130,130);
+	Color lightpanel = new Color(220,220,220);
+	Color lightsubpanel = new Color(190,190,190);
+	Color lightcomponents = new Color(180,180,180);
+}
